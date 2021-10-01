aa44155dcf6f6d958f833afc937e1dccc0f43fa4
Alexander Isett <alexander.isett@g.austincc.edu>
2021-09-30 21:15:40 -0500

    class 09/30

--- test/examples/08_classes/08_classes_tests.cpp
+++ test/examples/08_classes/08_classes_tests.cpp
@@ -1,12 +1,26 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "bank_account.h"
+
+
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
-TEST_CASE("Test account get balance")
+// TEST_CASE("Test account get balance")
+// {
+// 	Account account;
+// 	REQUIRE(account.getbalance(0) == 0);
+// }
+
+TEST_CASE("test account get balance with 1 parameter")
 {
-	Account account;
-		REQUIRE(account.getbalance(0) == 0);
+	Account account(100);
+	REQUIRE(account.get_balance() == 100);
+}
+
+TEST_CASE("test account deposit with 1 parameter")
+{
+	Account account(100);
+	REQUIRE(account.get_balance() == 100);
 }
