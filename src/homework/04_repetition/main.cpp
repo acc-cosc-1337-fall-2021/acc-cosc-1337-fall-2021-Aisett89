74ac74f9896a72f8de442881939e0a1412243286
Alexander Isett <alexander.isett@g.austincc.edu>
2021-10-08 22:23:10 -0500

    hw-05-dna_completed

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,7 +1,8 @@
 //write include statements
-
+#include "dna.h"
+#include<iostream>
 //write using statements
-
+using std::cout; using std::cin; using std::string; 
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +13,51 @@ user enters a y or Y.
 */
 int main() 
 {
+	string dna;
+	
+	auto exit_confirmation = 'y';
+	
+	do{
+		auto user_input = 3;
+		cout<<"\n ***MAIN MENU ***\n"
+		"\nType 1 for GC Content."<<
+		"\nType 2 for DNA Compliment"<<
+		"\nType 3 to exit.\n"
+		"\nType number here: ";
+		cin>>user_input;
+
+		switch(user_input){
+		case 1:
+			cout<<"Type your content here: "; 
+			cin>>dna;
+			cout<<"\nYour content is: "<<get_gc_content(dna)<<"\n\n";
+			break;
+
+		case 2: 
+			cout<<"Type your compliments here: "; 
+			cin>>dna;
+			cout<<"\nYour original is: "<<dna<<"\n";
+			cout<<"\nYour reverse compliment is: "<<get_dna_complement(dna)<<"\n\n";
+			break;
+// Your complimentary dna code is: 
+		case 3:
+			cout<<"Are you sure you'd like to exit?\n"<<
+			"Type answer here (y/n): ";
+			cin>>exit_confirmation;
+			if (exit_confirmation == 'y' || exit_confirmation == 'Y'){
+				exit_confirmation = '\0';
+			}
+
+			else{
+				exit_confirmation = 'y';
+			}
+			break;			
+		default:
+			cout<<"\nInvalid entry\n";
+		}
+
+	}while (exit_confirmation == 'y');
+	
+
 	return 0;
 }
--- test/examples/04_repetition/04_repetition_tests.cpp
+++ test/examples/04_repetition/04_repetition_tests.cpp
@@ -1,6 +1,19 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "dna.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+TEST_CASE("Verify content tests for hw-5", "verification") {
+	REQUIRE(get_gc_content("AGCTATAG") == .375);
+	REQUIRE(get_gc_content("CGCTATAG") == .50);
+}
+TEST_CASE("Verify reverse tests for hw-5", "verification") {
+	REQUIRE(reverse_string("AAAACCCGGT") == "ACCGGGTTTT");
+	REQUIRE(reverse_string("CCCGGAAAAT") == "ATTTTCCGGG");
+}
+TEST_CASE("Verify compliment tests for hw-5", "verification") {
+	REQUIRE(get_dna_complement("AGCTATAG") == "GATATCGA");
+	REQUIRE(get_dna_complement("CGCTATAG") == "GATATCGC");
 }
