3cf54e63c8d4a76326c55b39472bacdc70cf5e81
Alexander Isett <89616058+Aisett89@users.noreply.github.com>
2021-10-22 22:49:02 -0500

    Update tic_tac_toe.h
    manually adding from github

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,20 @@
-//h
+#include<iostream>
+#include <vector>
+using std::string;
+using std::vector;
+
+class TicTacToe
+{
+    public:
+        bool game_over();
+        void start_game(string first_player);
+        void mark_board(int position);
+        void display_board() const;
+        string get_player() const;
+    private:
+        string player;
+        static vector <string> pegs;
+        bool check_board_full();
+        void clear_board();
+        void set_next_player();
+};
