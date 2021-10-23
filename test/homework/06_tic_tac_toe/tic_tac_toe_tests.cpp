bc0ec0de968152fe8c687b4fe2a415a75266b336
Alexander Isett <alexander.isett@g.austincc.edu>
2021-10-22 23:23:25 -0500

    hw-06-final

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -1,7 +1,36 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "tic_tac_toe.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+
+TEST_CASE("Verify Game")
+{
+	TicTacToe ttt;
+	string first_player = "X";
+	int position;
+	int arr[9] = {1,2,3,4,5,7,6,9,8};
+	int counter;
+	void start_game(string first_player);
+	ttt.display_board();
+	do
+	{
+		for (counter = 0; counter < 9; counter++)
+		{
+			position = arr[counter];
+			ttt.mark_board(position);
+			ttt.display_board();
+		}
+		if ( counter < 9)
+		{
+			REQUIRE(ttt.game_over() == false);
+		}
+		else
+		{
+			REQUIRE(ttt.game_over() == true);
+		}
+	} while ( ttt.game_over() != true );
+}
